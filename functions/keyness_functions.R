1459a7cfb05b9052969af80f3436d2c66f79af96
browndw <dwb2@andrew.cmu.edu>
2021-01-25 14:25:34 -0500

    Update keyness_functions.R

--- functions/keyness_functions.R
+++ functions/keyness_functions.R
@@ -6,9 +6,10 @@
 log_like <- function(frequency_a, frequency_b, total_a, total_b) { 
   expected_a <- total_a*((frequency_a+frequency_b)/(total_a+total_b))
   expected_b <- total_b*((frequency_a+frequency_b)/(total_a+total_b))
-  L1 <- if(frequency_a == 0) 0 else (frequency_a*log(frequency_a/expected_a))
-  L2 <- if(frequency_b == 0) 0 else (frequency_b*log(frequency_b/expected_b))
+  L1 <- ifelse(frequency_a == 0, 0, frequency_a*log(frequency_a/expected_a))
+  L2 <- ifelse(frequency_b == 0, 0, frequency_b*log(frequency_b/expected_b))
   likelihood <- 2*(L1 + L2)
+  likelihood <- ifelse(frequency_b > frequency_a, -1*likelihood, likelihood)
   likelihood <- round(likelihood, 2)
   return(likelihood)
 }
@@ -18,8 +19,8 @@ log_like <- function(frequency_a, frequency_b, total_a, total_b) {
 # And was proposed by Andrew Hardie: http://cass.lancs.ac.uk/log-ratio-an-informal-introduction/
 
 log_ratio <- function(frequency_a, frequency_b, total_a, total_b) { 
-  percent_a <- if(frequency_a == 0) (.5/total_a) else (frequency_a/total_a)
-  percent_b <- if(frequency_b == 0) (.5/total_b) else (frequency_b/total_b)
+  percent_a <- ifelse(frequency_a == 0, .5/total_a, frequency_a/total_a)
+  percent_b <- ifelse(frequency_b == 0, .5/total_b, frequency_b/total_b)
   ratio <- (log2(percent_a/percent_b))
   ratio <- round(ratio, 2)
   return(ratio)
